f47dc000e2a51d97bab40742ba50a418f3517e16
Niyazov <diyar.niyazov08@gmail.com>
2026-02-03 12:14:28 -0500

    add: Correlation Coefficient

--- Video Game Sales/src/Analyzer.java
+++ Video Game Sales/src/Analyzer.java
@@ -10,8 +10,33 @@ public class Analyzer {
 
     public void parseCSV() throws IOException {
         Scanner in = new Scanner(csvFile);
-        while(in.hasNextLine()) {
-
+        String line;
+        while (in.hasNextLine()) {
+            line = in.nextLine();
         }
+    }
+
+    /*
+     * Pearson's Correlation Coefficient Formula
+     *
+     * The most commonly used formula for calculating the correlation coefficient is
+     * Pearson's Correlation Coefficient, given by:
+     *
+     * r = [n(Σxy) - (Σx)(Σy)] / √{[n(Σx²) - (Σx)²] [n(Σy²) - (Σy)²]}
+     * Copy
+     * Where:
+     *
+     * n is the number of data points.
+     *
+     * Σxy is the sum of the product of corresponding x and y values.
+     *
+     * Σx and Σy are the sums of x and y values, respectively.
+     *
+     *Σx² and Σy² are the sums of the squares of x and y values.
+     */
+    
+    public int correlationCoefficient(Game[] data) {
+
+        return 0;
     }
 }
