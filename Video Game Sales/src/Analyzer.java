4700d0bbffddf89eef9864db23c718e5b594a8ae
in4418 <inovak4242@gmail.com>
2026-02-03 12:14:39 -0500

    worked on analyzer parse csv

--- Video Game Sales/src/Analyzer.java
+++ Video Game Sales/src/Analyzer.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Analyzer {
-    private static Game[] data;
+    private static ArrayList<Game> data = new ArrayList<>();
     private static final File csvFile = new File("Video Game Sales\\assets\\vgsales.csv\\vgsales.csv");
 
     public void parseCSV() throws IOException {
         Scanner in = new Scanner(csvFile);
+        String line;
+        String[] list;
         while(in.hasNextLine()) {
-
+            line = in.nextLine();
+            list = line.split(",");
         }
     }
 }
