338d131534560cd20d9288caa514ae59f2a046a7
Diyar Niyazov <diyar.niyazov08@gmail.com>
2026-02-04 14:37:42 -0500

    Update
    remove double vgsales.csv folder
    modify parseCSV method
    add regex for initializing Game.java
    create Game.toString method

--- Video Game Sales/src/Analyzer.java
+++ Video Game Sales/src/Analyzer.java
@@ -1,25 +1,29 @@
+
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Analyzer {
-    private static Game[] games = new Game[16600];
-    private static final File csvFile = new File("Video Game Sales\\assets\\vgsales.csv\\vgsales.csv");
+
+    private static final Game[] games = new Game[16600];
+    private static final File CSV_FILE = Paths.get("..", "assets", "vgsales.csv").toFile();
 
     public void parseCSV() throws IOException {
-        try (Scanner in = new Scanner(csvFile)) {
+        System.out.println(CSV_FILE.toString());
+        try (Scanner in = new Scanner(CSV_FILE)) {
             String line;
-            String[] game_values = new String[11];
+            int count = 0;
             while (in.hasNextLine()) {
                 line = in.nextLine();
-                game_values = line.split(",");
-                System.out.println(Arrays.toString(game_values));
-                for(Game game : games) {
-                    game = new Game(game_values);
-                }
+                games[count] = new Game(line.split(","));
+                count++;
             }
+            for (Game game : games) {
+                System.out.println(game.toString());
+            }
+        } catch (IOException e) {
+            System.err.println("File doesn't exist.");
         }
     }
 
--- Video Game Sales/src/Game.java
+++ Video Game Sales/src/Game.java
@@ -1,36 +1,60 @@
-// Created by Diyar Niyazov and Isaac Novak
+//vgsales.csv format:
 // Rank,Name,Platform,Year,Genre,Publisher,NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global_Sales
+
 public class Game {
-    private int rank;
-    private String name;
-    private String platform;
-    private int year;
-    private String genre;
-    private String publisher;
-    private double na_sales;
-    private double eu_sales;
-    private double jp_sales;
-    private double other_sales;
-    private double global_sales;
+
+    private final int rank;
+    private final String name;
+    private final String platform;
+    private final int year;
+    private final String genre;
+    private final String publisher;
+    private final double na_sales;
+    private final double eu_sales;
+    private final double jp_sales;
+    private final double other_sales;
+    private final double global_sales;
 
     public Game(String[] game_values) {
-        this.rank = Integer.parseInt(game_values[0]);
+        if (game_values[0].matches("^\\d+$")) {
+            this.rank = Integer.parseInt(game_values[0]);
+        } else {
+            this.rank = -1;
+        }
         this.name = game_values[1];
         this.platform = game_values[2];
-        if (!game_values[3].equals("N/A"))
+        if (game_values[3].matches("^\\d+$")) {
             this.year = Integer.parseInt(game_values[3]);
+        } else {
+            this.year = -1;
+        }
         this.genre = game_values[4];
         this.publisher = game_values[5];
-        if (!game_values[6].equals("N/A"))
+        if (game_values[6].matches("^\\d+$|^\\d+\\.\\d+$")) {
             this.na_sales = Double.parseDouble(game_values[6]);
-        if (!game_values[7].equals("N/A"))
+        } else {
+            this.na_sales = -1;
+        }
+        if (game_values[7].matches("^\\d+$|^\\d+\\.\\d+$")) {
             this.eu_sales = Double.parseDouble(game_values[7]);
-        if (!game_values[8].equals("N/A"))
+        } else {
+            this.eu_sales = -1;
+        }
+        if (game_values[8].matches("^\\d+$|^\\d+\\.\\d+$")) {
             this.jp_sales = Double.parseDouble(game_values[8]);
-        if (!game_values[9].equals("N/A"))
+        } else {
+            this.jp_sales = -1;
+        }
+        if (game_values[9].matches("^\\d+$|^\\d+\\.\\d+$")) {
             this.other_sales = Double.parseDouble(game_values[9]);
-        if (!game_values[10].equals("N/A"))
+        } else {
+            this.other_sales = -1;
+        }
+        if (game_values[10].matches("^\\d+$|^\\d+\\.\\d+$")) {
             this.global_sales = Double.parseDouble(game_values[10]);
+        } else {
+            this.global_sales = -1;
+        }
     }
 
     public String getName() {
@@ -75,5 +99,23 @@ public class Game {
 
     public double getGlobal_sales() {
         return global_sales;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        result += "Rank: " + rank;
+        result += "\nName: " + name;
+        result += "\nPlatform: " + platform;
+        result += "\nYear: " + year;
+        result += "\nGenre: " + genre;
+        result += "\nPublisher: " + publisher;
+        result += "\nNA Sales: " + na_sales;
+        result += "\nEU Sales: " + eu_sales;
+        result += "\nJP Sales: " + jp_sales;
+        result += "\nOther Sales: " + other_sales;
+        result += "\nGlobal Sales: " + global_sales;
+        result += "\n";
+        return result;
     }
 }
