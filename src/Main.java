a0421579111906b24aeb64adff7bd3a88c8a9bbe
in4418 <inovak4242@gmail.com>
2026-02-05 12:13:34 -0500

    jlhsdbfljhadb

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,30 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Analyzer analyzer = new Analyzer();
-        analyzer.parseCSV();
-        analyzer.calculateCorrelationCoefficient();
+        //Analyzer analyzer = new Analyzer();
+        //analyzer.parseCSV();
+        //double[][] correlationCoefficients = analyzer.calculateCorrelationCoefficient();
+        display(new double[][] {{-1, 0.3, 0.6}, {0.4, -1, 0.7}, {0.1, 0.9, -1}}, new String[] {"valelelelel1", "val2", "val3"});
+    }
+
+    private static void display(double[][] corrCoeffs, String[] variableNames) {
+        System.out.print("\t");
+        for(int i = 0; i < variableNames.length; i ++) {
+            if(variableNames[i].length() >= 8) {
+                variableNames[i] = variableNames[i].substring(0, 4) + "...";
+            }
+            System.out.print("|\t" + variableNames[i] + "\t");
+        }
+        for(int i = 0; i < corrCoeffs.length; i ++) {
+            System.out.println();
+            System.out.print(variableNames[i] + "\t");
+            for(int j = 0; j < corrCoeffs[i].length; j ++) {
+                if(i == j) {
+                    System.out.print("|\txxx\t");
+                } else {
+                    System.out.print("|\t" + corrCoeffs[i][j] + "\t");
+                }
+            }
+        }
     }
 }
