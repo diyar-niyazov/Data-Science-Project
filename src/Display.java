cb9246b9fb023c664d7389830a34cfa46eea3aee
in4418 <inovak4242@gmail.com>
2026-02-09 12:14:18 -0500

    Added checks for invalid parameters, added dynamic string sizes

--- src/Display.java
+++ src/Display.java
@@ -15,22 +15,52 @@ public class Display {
         draw(corrCoeffs, valueNames);
     }
     public static void draw(double[][] corrCoeffs, String[] valueNames) {
-        //Setup graphics
+        //Check invalid parameters
         int width = 1800;
-        int height = 900;
+        int height = 1080;
+        int numValues = valueNames.length;
+
+        if(width % (numValues + 1) != 0) {
+            System.out.println("\nInvalid width!\nMake sure your width is divisible by the number of cells wide.\nCurrent width: " + width + "\nTry some of the following values instead: ");
+            for(int i = 50; i < 200; i += 10) {
+                System.out.print((numValues + 1) * i + "   ");
+            }
+            return;
+        }
+        if(height % (numValues + 1) != 0) {
+            System.out.println("\nInvalid height!\nMake sure your height is divisible by the number of cells wide.\nCurrent height: " + height + "\nTry some of the following values instead: ");
+            for(int i = 50; i < 200; i += 10) {
+                System.out.print((numValues + 1) * i + "   ");
+            }
+            return;
+        }
+
+        if(corrCoeffs == null) {
+            System.out.println("\nCorrelation coefficient array is null.");
+            return;
+        }
+
+        if(corrCoeffs.length != corrCoeffs[1].length || corrCoeffs.length != valueNames.length) {
+            System.out.println("\nDifferent amount of values and correlation coefficients.");
+            return;
+        }
+
+        //Setup graphics
         DrawingPanel panel = new DrawingPanel(width, height);
         Graphics g = panel.getGraphics();
         Font font = new Font("Arial", Font.BOLD, 15);
         g.setFont(font);
-        int numValues = valueNames.length;
+
         int cellWidth = width / (numValues + 1);
         int cellHeight = height / (numValues + 1);
         panel.setBackground(new Color(0, 0, 0));
 
+
         //Fix value name lengths
+        int maxLength = cellWidth / 8;
         for(int i = 0; i < numValues; i ++) {
-            if(valueNames[i].length() > 13) {
-                valueNames[i] = valueNames[i].substring(0, 11) + "...";
+            if(valueNames[i].length() > maxLength) {
+                valueNames[i] = valueNames[i].substring(0, maxLength - 2) + "...";
             }
         }
 
@@ -47,7 +77,7 @@ public class Display {
         for(int i = 0; i < corrCoeffs.length; i ++) {
             for(int j = 0; j < corrCoeffs[i].length; j ++) {
                 if(i == j) {
-                    g.setColor(new Color(255, 255, 0));
+                    g.setColor(new Color(150, 150, 150));
                     g.fillRect((i * cellWidth) + cellWidth, (j * cellHeight) + cellHeight, cellWidth, cellHeight);
                     continue;
                 }
